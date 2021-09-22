903ec76ef557a6314044b43e78827380307019e2
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-09-22 09:20:35 -0300

    desafio feito

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,25 @@
+import { useContext } from 'react';
+
+import { MovieCard } from './MovieCard';
+
+import { Context } from '../Context';
+
 export function Content() {
-  // Complete aqui
+  const { movies, selectedGenre } = useContext(Context)
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
